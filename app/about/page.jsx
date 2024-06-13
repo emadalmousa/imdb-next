00ced57fa8423b3f5afc8827297483917dda44ad
Emad Almousa <almousa.emad.92@gmail.com>
2024-06-13 11:00:38 +0200

    img to Image ändern

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
+const Page = () => {
   return (
-    <div className='max-w-6xl mx-auto p-3 space-y-4' >
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
       <h1 className='text-2xl font-medium text-amber-600'>About</h1>
-      <p >
+      <p>
         At Comany XXX, our mission is to revolutionize the way people interact with technology.
         We believe in creating innovative solutions that simplify daily tasks and enhance productivity.
         Our team of dedicated professionals works tirelessly to develop cutting-edge products that not
@@ -12,15 +12,13 @@ const page = () => {
         technological advancements, ensuring that our users always have access to the best tools available.
       </p>
 
-
       <p>
         We are committed to maintaining the highest standards of integrity and transparency in all our operations.
         Our core values are rooted in a strong commitment to quality, customer satisfaction, and continuous
-        improvement. We believe in fostering a culture of collaboration and respect, where every team member's
+        improvement. We believe in fostering a culture of collaboration and respect, where every team member&apos;s
         contribution is valued. By prioritizing ethical practices and sustainable growth, we aim to create
         a positive impact on our community and the world at large.
       </p>
-
 
       <p>Looking ahead, our vision is to be a global leader in the tech industry, known for our innovation,
         reliability, and excellence. We aim to expand our product portfolio and reach new markets, while
@@ -28,7 +26,7 @@ const page = () => {
         confident that we can achieve long-term success and make a meaningful difference in the lives of
         our users. Join us on this exciting journey as we continue to push the boundaries of what is possible.</p>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 
 const loading = () => {
   return (
     <div className='flex justify-center'>
-        <img src="spinner.svg"/>
+        <Image src="/spinner.svg" alt="Loading spinner" width={500} height={500} />
     </div>
   )
 }
